fff8a6e21f9b72de9bd23b7aff534577f3e12dee
sorryaboutyourcats <sorryaboutyourcats@gmail.com>
2019-12-19 11:43:40 -0800

    Added ability to switch scenes, breaks webcam though

---
+++ Assets/SceneMan.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMan : MonoBehaviour
+{
+    void Awake()
+    {
+        int numSceneMan = FindObjectsOfType<SceneMan>().Length;
+        if (numSceneMan > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    //void Start()
+    //{
+    //    SceneManager.LoadScene(sceneBuildIndex:0);
+    //}
+
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            SceneManager.LoadScene(sceneBuildIndex:0);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            SceneManager.LoadScene(sceneBuildIndex:1);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            SceneManager.LoadScene(sceneBuildIndex:2);
+        }
+    }
+
+}
