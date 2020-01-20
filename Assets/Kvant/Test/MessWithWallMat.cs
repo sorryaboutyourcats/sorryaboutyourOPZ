ab0030fca8bda19268dda867d6a8382ba02c0c96
sorryaboutyourcats <sorryaboutyourcats@gmail.com>
2020-01-20 13:19:13 -0800

    Messing with shaders

---
+++ Assets/Kvant/Test/MessWithWallMat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessWithWallMat : MonoBehaviour
+{
+    public Renderer holoRenderer;
+
+    void Start()
+    {
+        holoRenderer = GetComponent<Renderer>();
+        holoRenderer.material.SetFloat("_Amount", 1f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
